5da6e84a6851a64878a8551a3eada7e772697605
Michael Rivera <soymrmike@gmail.com>
2018-10-10 20:55:22 -0700

    can operate YouTube player with API.

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,17 +1,62 @@
 
+var ytApiLoadedHook = null;
+
+function onYouTubeIframeAPIReady() {
+  ytApiLoadedHook()
+}
+
 class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      player: null,
+      done: false
+    }
+
+    this.onPlayerReady = this.onPlayerReady.bind(this)
+    this.onPlayerStateChange = this.onPlayerStateChange.bind(this)
+  }
+
+  onPlayerReady(e) {
+    e.target.playVideo()
+  }
+  
+  onPlayerStateChange(e) {
+    if(e.data === YT.PlayerState.PLAYING && !this.done) {
+      setTimeout(() => this.player.stopVideo(), 6000)
+      this.setState({done: true})
+    }
+  }
+  
   componentDidMount() {
-    const html = `
-        <iframe id="ytplayer" type="text/html" width="720" height="405"
-         src="https://www.youtube.com/embed/QLkSoyQDOXo"
-         frameborder="0" allowfullscreen>
-`
-    this.el.innerHTML = html    
+    // bind to ytApiLoadedHook before onYouTubeIframeAPIReady tries
+    // to use it.
+    window.ytApiLoadedHook = () => {
+      this.player = new YT.Player('embedded-player-5', {
+        height: '390',
+        width: '640',
+        videoId: 'QLkSoyQDOXo',
+        events: {
+          'onReady': this.onPlayerReady,
+          'onStateChange': this.onPlayerStateChange
+        },
+        origin: window.location.origin
+      })      
+    }
+
+    let tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    const firstScriptTag = document.getElementsByTagName('script')[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)    
   }
   
   render() {
-    return (<div ref={el => this.el = el}></div>)
+    return (
+      <div className="embedded-player-container">
+        <div id="embedded-player-5"></div>
+      </div>
+    )
   }
 }
 
