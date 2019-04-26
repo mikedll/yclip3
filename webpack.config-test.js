f0bee187c614cc4ddc566cb1896333de8e0ec066
Michael Rivera <soymrmike@gmail.com>
2019-04-26 02:59:10 -0700

    Using mochapack sincem mocha-webpack is not being maintained.

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -2,6 +2,7 @@ var nodeExternals = require('webpack-node-externals');
 const path = require('path')
 
 module.exports = {
+  mode: "development",
   target: 'node',
   externals: [nodeExternals()],
   output: {
