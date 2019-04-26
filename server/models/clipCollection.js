642b801fb08018a428d7709a59ff8886250b1748
Michael Rivera <soymrmike@gmail.com>
2019-04-25 18:37:12 -0700

    ClipCollection model and spec for creating new clips.

---
+++ server/models/clipCollection.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose')
+
+const ClipSchema = new mongoose.Schema({
+  vid: {
+    type: String,
+    required: true
+  },
+  start: {
+    type: Number,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  }
+})
+
+const ClipCollectionSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+  clips: {
+    type: [ClipSchema]
+  }
+  
+})
+
+const ClipCollection = mongoose.model('ClipCollection', ClipCollectionSchema)
+
+export default ClipCollection
--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,25 +1,35 @@
 
+const request = require('supertest')
 const path = require('path')
 const srcDir = path.join(__dirname, '../server/')
 const app = require(path.join(srcDir, 'app.js'))
 const mongoose = require('mongoose')
-const ClipCollection = require(path.join(srcDir, 'models/collection.js'))
+const ClipCollection = require(path.join(srcDir, 'models/clipCollection.js'))
   
 describe('App', () => {
 
+  let clip1 = {
+    vid:"Iwuy4hHO3YQ",
+    start: 34,
+    duration: 3
+  }
+  
   beforeAll(() => {
-    mongoose.connect(config.mongo.uri, config.mongo.collectionOpts)
+    return mongoose.connect(config.mongo.uri, config.mongo.collectionOpts)
   })
+
+  afterAll(() => { return mongoose.disconnect() } )
   
   test('it should save a new clip in a collection', () => {
     const collection = new ClipCollection({name: "nice songs"})
     return collection.save()
       .then(collection => {
-        app.post('/collections/' + collection._id + '/clips')
+        request(app).post('/collections/' + collection._id + '/clips', clip1)
       })
-      .then(() => {
+      .then((response) => {
         const collectionUpdated = loadCollection(collection.id)
         expect(collectionUpdated.clips.length).toBe(1)
+        expect(collectionUpdated.clips[0].vid).toBe("Iwuy4hHO3YQ")
       })
   })
 })
