f7c0d000d4c7cd53a42b6a788a092fa14e0c5c0a
Michael Rivera <soymrmike@gmail.com>
2019-06-06 21:35:00 -0700

    Inlude createdAt in clips migration.

--- server/migrations/20190526205852_move_clips.js
+++ server/migrations/20190526205852_move_clips.js
@@ -17,6 +17,11 @@ const ClipSchema = new mongoose.Schema({
   duration: {
     type: Number,
     required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
   }
 })
 
